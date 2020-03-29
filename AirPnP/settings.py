f14c64a1dc26d2994a64dfa4b6c9cb1c91c2aa82
warbrain11 <brandonberke@gmail.com>
2020-03-28 20:59:10 -0400

    Added whitelist and allowed hosts

--- AirPnP/settings.py
+++ AirPnP/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '=ww*6cujkcsgs7p_07*@7x_h2+al1ytbw7$e_^cy!5!221=6-p'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+#ALLOWED_HOSTS = ['127.0.0.1', '127.0.1.1', '96.250.36.86', 'www.airpnpbcs430w.info']
 
 # Application definition
 
@@ -55,6 +55,10 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+#CORS_ORIGIN_WHITELIST = [ 'http://localhost:3000/', 'http://96.250.36.86:3000', 'https://96.250.36.86:3000', 'https://www.airpnpbcs430w.info/', 'https://www.airpnpbcs430w.info/:3000', ]
+
+
 
 ROOT_URLCONF = 'AirPnP.urls'
 
